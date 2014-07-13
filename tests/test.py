3b8fd17e692465c74b0fdbf0da3ad01e6d587069
petit-prince <felix.leblanc1305@gmail.com>
2014-07-13 15:20:25 +0000

    Fixed fixture creation and deletion.

--- tests/test.py
+++ tests/test.py
@@ -2,21 +2,23 @@
 import unittest
 from rune.database import database 
 import os.path
+from rune.schema import schema
 
 
 class Rune_tests(unittest.TestCase):
-
+    def setUp(self):
+        self.filename = 'test.rune'
+        db = database(self.filename)
+    def tearDown(self):
+        if os.path.isfile(self.filename):
+            os.remove(self.filename)
     def testMethod(self):
         self.assertEqual(3, 1+2)
     def testMethod2(self):
         self.assertEqual(3, 2+1)
     def testMethod3(self):
         self.assertEqual(3, 2+1)
-
     def test_db_init(self):
-        filename = 'test.rune'
-        db = database(filename)
-        self.assertTrue(os.path.isfile(filename))
-
+        self.assertTrue(os.path.isfile(self.filename))
 if __name__ == '__main__':
     unittest.main()
