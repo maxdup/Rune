fa0bf0b5a4b1b0a7813b6e4d4e3848357098f2ae
petit-prince <felix.leblanc1305@gmail.com>
2014-07-13 17:57:19 +0000

    Removed unecessary file and renamed test class name

--- tests/test.py
+++ tests/test.py
@@ -6,7 +6,7 @@ from rune.database import database
 from rune.schema import schema
 
 
-class Rune_tests(unittest.TestCase):
+class DatabaseTest(unittest.TestCase):
     def setUp(self):
         self.filename = 'test.rune'
         self.db = database(self.filename)
@@ -15,13 +15,11 @@ class Rune_tests(unittest.TestCase):
             os.remove(self.filename)
     def test_db_init(self):
         self.assertTrue(os.path.isfile(self.filename))
-
     def test_add_schema(self):
         schema1 = schema(1)
         schema1.setSchema(['int'])
         self.db.add_schema(schema1)
         self.assertEqual(1, len(self.db.schemas))
-
 
 if __name__ == '__main__':
     unittest.main()
